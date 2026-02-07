7dd513900493bf171631c1431e335ab38f723bf5
kozhobekov16 <zhakshylykkozhobekov@gmail.com>
2026-02-07 18:44:05 +0600

    WIP: module users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersService } from './users/users.service';
+import { AuthService } from './auth/auth.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService, AuthService],
 })
-export class AppModule {}
+export class AppModule {
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const { email, password } = createUserDto;
+    const existing = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    if (existing) {
+      throw new ConflictException('User already exists');
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+  }
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+}
